6b72725dbff5d6d0752d247da420265363b9a21e
allyn <allyn.alda@gmail.com>
2020-11-18 18:01:42 +0100

    goback to history

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from "react-router-dom";
+import { IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,9 +18,16 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header() {
     const classes = useStyles();
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push(`/`);
+    }
     return (
         <div className={classes.root}>
+            <IconButton onClick={handleClick}>
             <img className={classes.image} src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png" alt="logo"/>
+            </IconButton>
             <h3>Oompa Loopa's Crew</h3>
         </div>
     )
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStateValue } from '../state/state';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -10,6 +10,16 @@ const useStyles = makeStyles((theme) => ({
     image: {
         width: '100%'
     },
+    text: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '80%',
+        padding: '20px'
+    },
+    top: {
+        paddingTop: '20px'
+    }
   }));
 export default function Detail() {
     const classes = useStyles();
@@ -20,7 +30,12 @@ export default function Detail() {
                 <img alt="oompa" src={oompa.image} className={classes.image} />
             </Grid>
             <Grid item md={6}>
-            {oompa.first_name} {oompa.last_name}
+                <div className={classes.text}>
+            <Typography variant="h5">{oompa.first_name} {oompa.last_name}</Typography>
+            <Typography variant="body1">{oompa.gender === 'F' ? 'Woman' : 'Man'}</Typography>
+            <Typography variant="body1">{oompa.profession}</Typography>
+            <Typography variant="body1" className={classes.top}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+                </div>
             </Grid>
         </Grid>
     )
