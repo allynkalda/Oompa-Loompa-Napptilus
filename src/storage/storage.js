190be5dcae302f7eb48c3d3845e625735e2afac8
allyn <allyn.alda@gmail.com>
2020-11-23 09:54:11 +0100

    delete consoles add read me

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -3,62 +3,48 @@ const key = 'data';
 
 const setCachedData = (value) => {
 	const now = new Date();
-
-	// `item` is an object which contains the original value
-	// as well as the time when it's supposed to expire
 	const item = {
 		value: value,
 		expiry: now.getTime() + ttl,
     }
-	localStorage.setItem(key, JSON.stringify(item))
+	localStorage.setItem(key, JSON.stringify(item));
 }
 
 const getCachedData = () => {
 	const itemStr = localStorage.getItem(key);
-	// if the item doesn't exist, return null
 	if (!itemStr) {
-		return null
+		return null;
 	}
-	const item = JSON.parse(itemStr)
-	const now = new Date()
-	// compare the expiry time of the item with the current time
+	const item = JSON.parse(itemStr);
+	const now = new Date();
 	if (now.getTime() > item.expiry) {
-		// If the item is expired, delete the item from storage
-		// and return null
 		localStorage.removeItem(key)
-		return null
+		return null;
     }
-	return item.value
+	return item.value;
 }
 
 const setCachedOompa = (value, id) => {
 	const now = new Date();
-
-	// `item` is an object which contains the original value
-	// as well as the time when it's supposed to expire
 	const item = {
 		value: value,
 		expiry: now.getTime() + ttl,
     }
-	localStorage.setItem(`oompa-${id}`, JSON.stringify(item))
+	localStorage.setItem(`oompa-${id}`, JSON.stringify(item));
 }
 
 const getCachedOompa = (id) => {
-	const itemStr = localStorage.getItem(`oompa-${id}`)
-	// if the item doesn't exist, return null
+	const itemStr = localStorage.getItem(`oompa-${id}`);
 	if (!itemStr) {
 		return null
 	}
-	const item = JSON.parse(itemStr)
-	const now = new Date()
-	// compare the expiry time of the item with the current time
+	const item = JSON.parse(itemStr);
+	const now = new Date();
 	if (now.getTime() > item.expiry) {
-		// If the item is expired, delete the item from storage
-		// and return null
 		localStorage.removeItem(`oompa-${id}`)
-		return null
+		return null;
     }
-	return item.value
+	return item.value;
 }
 
 export { setCachedData, getCachedData, setCachedOompa, getCachedOompa };
